f970da5bacf3b8daeef0166618d20538711b2ec3
Carlos Fisac Ferrández <charles8ff@gmail.com>
2021-02-10 16:31:36 +0000

    first commit adding files

--- src/models.py
+++ src/models.py
@@ -3,17 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    task = db.relationship("Task", lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User: {self.name}'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            # do not serialize the password, its a security breach
+        }
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(120), unique=True, nullable=False)
+    is_done = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id= db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'Task: {self.label}'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.label,
             # do not serialize the password, its a security breach
         }
